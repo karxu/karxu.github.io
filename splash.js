3a87fd8cd03aa0bdaae50d44283619330a84fe96
Karen Xu <karenxu@uchicago.edu>
2016-10-23 15:57:10 +0100

    first upload of files

---
+++ splash.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $('#logo')
+        .animate({opacity: 0,}, 0)
+        .delay(5)
+        .animate({opacity: 1}, 2000);
+});
+
+$(document).ready(function() {
+    $('#dev')
+        .animate({opacity: 0,}, 0)
+        .delay(200)
+        .animate({opacity: 1}, 2500);
+});
+$(document).ready(function() {
+    $('#design')
+        .animate({opacity: 0,}, 0)
+        .delay(800)
+        .animate({opacity: 1}, 2500);
+});
+$(document).ready(function() {
+    $('#blog')
+        .animate({opacity: 0}, 0)
+        .delay(1200)
+        .animate({opacity: 1}, 2500);
+});
+
