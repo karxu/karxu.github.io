e4ac4b6c937d986c2a964b0648cfcc1328fe7a14
Karen Xu <karenxu@uchicago.edu>
2016-11-03 18:31:50 +0000

    created a projects page

---
+++ dev/dev.js
@@ -0,0 +1,24 @@
+var windw = this;
+
+$.fn.followTo = function ( pos ) {
+    var $this = this,
+        $window = $(windw);
+    
+    $window.scroll(function(e){
+        if ($window.scrollTop() < pos) {
+            $this.css({
+                position: 'absolute',
+                top: pos
+            });
+        } else {
+            $this.css({
+                position: 'fixed',
+                top: 0
+            });
+        }
+    });
+};
+
+$('#nav').followTo(1100);
+
+// $('body').scrollspy({ target: '#nav' });
